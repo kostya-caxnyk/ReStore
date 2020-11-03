11cd612e118225bad9f4cfd403467c8724c6c634
kostya <caxnyk2002@gmail.com>
2020-11-03 11:50:13 +0300

    Updates

---
+++ precache-manifest.056243ba440c53c96e838790cc362c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a303c630d4ac3d09f351796eeb79b8d",
+    "url": "/ReStore/index.html"
+  },
+  {
+    "revision": "95d0d92cde1fb3b6dc1a",
+    "url": "/ReStore/static/css/main.dda589bd.chunk.css"
+  },
+  {
+    "revision": "7a395bddbf892483fb66",
+    "url": "/ReStore/static/js/2.4e5c400b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ReStore/static/js/2.4e5c400b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d0d92cde1fb3b6dc1a",
+    "url": "/ReStore/static/js/main.7b6e7315.chunk.js"
+  },
+  {
+    "revision": "0ab9106e4c2d8fae9734",
+    "url": "/ReStore/static/js/runtime-main.269101e1.js"
+  }
+]);
